db8e596bee78674e0650bf5a8d35847ca168b361
chenzhe <chenzhe.729@bytedance.com>
2022-06-01 10:43:34 +0800

    Everything in lua

---
+++ lua/user/wilder.lua
@@ -0,0 +1,16 @@
+local wilder = require('wilder')
+
+wilder.setup({modes = {':', '/', '?'}})
+
+wilder.set_option('renderer', wilder.renderer_mux({
+    [':'] = wilder.popupmenu_renderer({
+        highlighter = wilder.basic_highlighter(),
+    }),
+    ['/'] = wilder.wildmenu_renderer({
+        highlighter = wilder.basic_highlighter(),
+    }),
+}))
+
+wilder.set_option('renderer', wilder.popupmenu_renderer({
+    pumblend = 20,
+}))
