618eff12ca6e3d99c75da7b3031350713aa303d7
chenzhe <chenzhe.729@bytedance.com>
2022-06-14 22:09:32 +0800

    Add lsp_signature

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -154,6 +154,11 @@ _G.packer_plugins = {
     path = "/Users/admin/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
     url = "https://github.com/lukas-reineke/indent-blankline.nvim"
   },
+  ["lsp_signature.nvim"] = {
+    loaded = true,
+    path = "/Users/admin/.local/share/nvim/site/pack/packer/start/lsp_signature.nvim",
+    url = "https://github.com/ray-x/lsp_signature.nvim"
+  },
   ["lualine.nvim"] = {
     loaded = true,
     path = "/Users/admin/.local/share/nvim/site/pack/packer/start/lualine.nvim",
