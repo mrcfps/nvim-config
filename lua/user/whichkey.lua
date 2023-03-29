edd883dbd9714482a6a4df11ce60d48b853297eb
chenzhe <chenzhe.729@bytedance.com>
2023-03-29 19:52:50 +0800

    Optimize toggleterm size

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -182,8 +182,8 @@ local mappings = {
     t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
     p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
     f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
-    h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
-    v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
+    h = { "<cmd>ToggleTerm direction=horizontal<cr>", "Horizontal" },
+    v = { "<cmd>ToggleTerm direction=vertical<cr>", "Vertical" },
   },
 
   u = {
