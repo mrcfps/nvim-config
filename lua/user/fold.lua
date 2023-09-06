672c03da6de1d4b3aad691bfdfe5f8421cbc5392
chenzhe <chenzhe.729@bytedance.com>
2023-09-06 15:29:38 +0800

    Optimize initial folding

--- lua/user/fold.lua
+++ lua/user/fold.lua
@@ -3,6 +3,7 @@ local opt = vim.opt
 
 -- We want to use folds regularly, but don’t want folds to be applied when you open a file
 vim.cmd [[set nofoldenable]]
+vim.cmd [[set foldlevelstart=20]]
 
 opt.foldmethod = "indent"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
