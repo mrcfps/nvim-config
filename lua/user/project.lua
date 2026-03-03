39e1f5d1c858d478fbcfd68bb683e4d40c714778
mrcfps <mrc@powerformer.com>
2026-03-03 19:05:48 +0800

    Disable automatic project cwd switching in monorepos

--- lua/user/project.lua
+++ lua/user/project.lua
@@ -12,7 +12,7 @@ project.setup {
   ---@usage set to true to disable setting the current-woriking directory
   --- Manual mode doesn't automatically change your root directory, so you have
   --- the option to manually do so using `:ProjectRoot` command.
-  manual_mode = false,
+  manual_mode = true,
 
   ---@usage Methods of detecting the root directory
   --- Allowed values: **"lsp"** uses the native neovim lsp
@@ -23,7 +23,7 @@ project.setup {
   detection_methods = { "pattern" },
 
   ---@usage patterns used to detect root dir, when **"pattern"** is in detection_methods
-  patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+  patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile" },
 
   ---@ Show hidden files in telescope when searching for files in a project
   show_hidden = false,
